03b5593d2c3f0d06f30c28995b496bb46b8dcd84
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-21 23:11:06 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -12,7 +12,7 @@ def add_character(show, name)
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 
-
+array
   
   # Write your implementation here
   # Should return the array of the 'show' argument
